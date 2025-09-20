0d37513a5c2d61bc16582863015f1f82697ebebd
Luís Alves <luisgalvessilva@gmail.com>
2025-09-20 15:44:12 -0300

    Fix firebase config para prod

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -11,7 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-
 export default app;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
   server: {
     port: 5173,
     host: true
@@ -20,11 +22,16 @@ export default defineConfig({
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom'],
-          ui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-          firebase: ['firebase'],
+          ui: ['@mui/material', '@mui/icons-material'],
           utils: ['axios', 'react-hook-form']
         }
       }
     }
+  },
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/auth'
+    ]
   }
 })
