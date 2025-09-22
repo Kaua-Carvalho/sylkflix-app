aa35bad498dc5b6dea9bc00338aa2a860be6f41d
Luís Alves <luisgalvessilva@gmail.com>
2025-09-21 22:12:47 -0300

    Teste home - sem usar movieapi

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
-import { movieApi } from '../../services/tmdbApi';
+import { Container, Typography } from '@mui/material';
+// import { movieApi } from '../../services/tmdbApi'; // COMENTADO
 
 const Home = () => {
-  const handleClick = async () => {
-    try {
-      console.log('Testando API...');
-      const response = await movieApi.getGenres();
-      console.log('Sucesso:', response);
-    } catch (err) {
-      console.error('Erro:', err);
-    }
-  };
-
   return (
     <Container maxWidth="xl" sx={{ py: 4 }}>
-      <Typography variant="h3" sx={{ color: 'white', textAlign: 'center', mb: 3 }}>
-        Home - Teste Manual
+      <Typography variant="h3" sx={{ color: 'white', textAlign: 'center' }}>
+        Home - Sem usar movieApi
       </Typography>
-      
-      <Button 
-        variant="contained" 
-        onClick={handleClick}
-        sx={{ display: 'block', mx: 'auto' }}
-      >
-        Testar API
-      </Button>
     </Container>
   );
 };
