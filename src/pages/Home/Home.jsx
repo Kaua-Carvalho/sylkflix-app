45179925d3eead5896c7415ab9fdcdd0c26ae9bb
Luís Alves <luisgalvessilva@gmail.com>
2025-09-21 21:59:36 -0300

    Teste home com tmdbApi Import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
-import axios from 'axios';
+import { movieApi } from '../../services/tmdbApi';
 
 const Home = () => {
   return (
     <Container maxWidth="xl" sx={{ py: 4 }}>
       <Typography variant="h3" sx={{ color: 'white', textAlign: 'center' }}>
-        Home - Com Axios Import
+        Home - Com tmdbApi Import
       </Typography>
     </Container>
   );
